e8dd4b3327f83e735abec798d182dd38ddffae13
Zach Krall <zach@zachkrall.com>
2020-01-09 17:56:50 -0500

    adding first draft of vue portal

---
+++ vue/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <!-- https://win98icons.alexmeub.com/ -->
+    <NavBar />
+    <div
+      style="margin-top:2rem;position:absolute;right:0;text-align:center;padding:1rem;"
+    >
+      <img src="./assets/computer.png" />
+      <br />MY COMPUTER <br /><br /><img src="./assets/note.png" />
+      <br />COMPUTATIONAL<br />
+      ART TIMELINE <br /><br /><img src="./assets/note.png" /> <br />ARPANET
+      SITES <BR /><BR /><img src="./assets/note.png" /> <br />OPAQUE TOOLS<BR /><BR /><img
+        src="./assets/note.png"
+      />
+      <br />ELEMENTS OF <br />GENERATIVE<br />ART MAKING
+    </div>
+
+    <div
+      style="position:absolute;top:4rem;left:4rem;height:200px;overflow:auto;width:400px;background-color:#fff;
+      border-top:10px solid #222;padding:1rem;box-sizing:border-box;"
+    >
+      Random question:
+      <br />__What would the internet look like today if everyone maintained
+      their own personal websites? <br /><br />__passive browsing vs. active
+      participation in the internet<br /><br />__would people invent their own
+      solutions to sharing information<br /><br /><br /><br />
+    </div>
+
+    <div
+      style="position:absolute;top:400px;left:250px;height:200px;overflow:auto;width:400px;background-color:#fff;
+      border-top:10px solid #222;padding:1rem;box-sizing:border-box;"
+    >
+      OPAQUE TOOLS:
+      <ul>
+        Hardware:
+        <li>M5 Anti-aircraft gun director</li>
+        <li>DRONES</li>
+        <li>STINGRAYS</li>
+        <br />Software:
+        <li>NEURAL NETWORKS (FACIAL RECOGNITION, PREDICTIVE POLICING)</li>
+        <LI>ADVERTISING</LI>
+      </ul>
+    </div>
+
+    <iframe
+      style="position:absolute;bottom:10px;left:5px;"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/s1i-dnAH9Y4"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "app",
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap");
+body {
+  font-family: "IBM Plex Mono", monospace;
+  font-weight: 100;
+  background: #999;
+}
+</style>
---
+++ vue/src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav>
+    <ul>
+      <li></li>
+      <li>File</li>
+      <li>Edit</li>
+      <li>View</li>
+    </ul>
+    <div>
+      {{ time }}
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      time: this.getTime()
+    }
+  },
+  methods: {
+    getTime(){
+      let now = (new Date);
+      let format = (i) => i.toString().padStart(2,'0');
+      return `${format(now.getHours())}:${format(now.getMinutes())}:${format(now.getSeconds())}`;
+    }
+  },
+  created(){
+    setInterval( ()=>this.time = this.getTime(), 100);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav{
+  box-sizing:border-box;
+  position:fixed;
+  top:0;left:0;
+  width:100vw;
+
+  line-height:20px;
+  background-color:#222;
+  color:#fff;
+  padding: 0.1em 0.5em;
+
+  display:flex;
+  align-items:center;
+  justify-content: space-between;
+}
+ul{
+  display:inline-block;
+  list-style:none;
+  margin:0;padding:0;
+}
+li{
+  display:inline-block;
+  list-style:none;
+  margin:0 1rem 0 0 ;padding:0;
+}
+</style>
